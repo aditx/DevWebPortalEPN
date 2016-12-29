c06f3c01b695f5c911c18bb38ce9a8cbed24dac9
aditx <klikadsene@gmail.com>
2016-12-29 16:57:15 +0700

    Update

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=9">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+<meta http-equiv="X-UA-Compatible" content="IE=9">
 </head>
 
 <body <?php body_class(); ?>>
